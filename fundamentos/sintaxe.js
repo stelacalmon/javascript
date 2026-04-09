a01b25b2a4086e43b5565108a5aca772a50f2cc9
Stela Calmon <stelanayara.s@hotmail.com>
2026-04-08 22:19:34 -0300

    variáveis

---
+++ fundamentos/sintaxe.js
@@ -0,0 +1,8 @@
+let nota1, nota2, media
+
+nota1 = 7
+nota2 = 8
+
+media = (nota1 + nota2) / 2
+
+console.log(media)
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,44 @@
+/**
+ * Estudo das variáveis 
+ * @author Stela Calmon
+ */
+
+console.clear () 
+console.log ("Estudo das variáveis")
+console.log ("")
+console.log("string")
+// a linha abaixo cria uma variável 
+let nome = "Stela Calmon"
+console.log(nome)
+// a linha abaixo mostra o tipo de variável 
+console.log(typeof (nome))
+console.log("")
+console.log("Variável do tipo Number")
+let idade = 24
+console.log(idade)
+console.log(typeof(idade))
+
+let peso = 50
+console.log(peso)
+
+let altura = 1.50
+console.log(altura)
+console.log("")
+console.log("Variável do tipo Boolean")
+let vip = true
+console.log(vip)
+console.log(typeof(vip))
+
+
+console.log("")
+console.log("-------------------------")
+console.log("----- Ficha do aluno ----")
+console.log("-------------------------")
+// a linha abaixo concatena (união) um texto com o conteudo da variável 
+//
+console.log("")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}kg`)
+console.log(`Altura: ${altura}m`)
+console.log(`Vip: ${vip}`)
